8e05cebf114ca5cd3ac357f7b7732d0c776e6f1c
Patosha <Patosha68@gmail.com>
2022-04-06 23:03:01 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+const Star = () => {
+    return (
+        <div>
+            star
+        </div>
+    )
+}
+
+const AccordionTitle = () => {
+    return (
+        <div>
+            <h3>Меню</h3>
+        </div>
+    )
+}
+
+const AccordionBody = () => {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
 }
 
 export default App;
